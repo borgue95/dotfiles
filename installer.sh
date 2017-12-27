d32189009b0850c4268e1f90af1346f6db1c21b0
Berenguer Montserrat <berenguer.95@gmail.com>
2017-12-27 20:54:43 +0100

    Fixed currend dir variable

--- installer.sh
+++ installer.sh
@@ -11,6 +11,7 @@ set -e -o errexit
 # xset playerctl amixer not found on Raspbery Pi
 sudo apt-get install -y -q git build-essential libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf automake libxcb-xrm-dev scrot imagemagick xdotool arandr rofi
 mkdir -p $(xdg-user-dir PICTURES)/screen_shots
+PWD=$(pwd)
 
 # where to put my dot files
 DOT_DIR=$HOME/.mydotfiles
@@ -21,7 +22,7 @@ mkdir -p $DOT_DIR/scripts
 
 # VIM
 sudo apt-get install -y -q vim
-cp $(pwd)/vim/vimrc $DOT_DIR/vim/vimrc
+cp $PWD/vim/vimrc $DOT_DIR/vim/vimrc
 mv $HOME/.vimrc $HOME/.vimrc_old
 ln -sf $DOT_DIR/vim/vimrc $HOME/.vimrc
 
@@ -49,20 +50,20 @@ cd
 
 # i3
 sudo apt-get install -y -q i3 i3blocks i3lock
-cp $(pwd)/i3/config $DOT_DIR/i3/config
+cp $PWD/i3/config $DOT_DIR/i3/config
 ln -sf $DOT_DIR/i3/config $HOME/.config/i3/config
-cp $(pwd)/i3/compton.conf $DOT_DIR/i3/compton.conf
-cp $(pwd)/i3/i3blocks.conf $DOT_DIR/i3/i3blocks.conf
-cp $(pwd)/i3/rofi.conf $DOT_DIR/i3/rofi.conf
-cp $(pwd)/i3/i3_init.sh $DOT_DIR/i3/i3_init.sh
+cp $PWD/i3/compton.conf $DOT_DIR/i3/compton.conf
+cp $PWD/i3/i3blocks.conf $DOT_DIR/i3/i3blocks.conf
+cp $PWD/i3/rofi.conf $DOT_DIR/i3/rofi.conf
+cp $PWD/i3/i3_init.sh $DOT_DIR/i3/i3_init.sh
 
 # scripts for i3blocks and other things
-cp $(pwd)/scripts/getmyip.sh $DOT_DIR/scripts/getmyip.sh
-cp $(pwd)/scripts/screenshot_selection.sh $DOT_DIR/scripts/screenshot_selection.sh
-cp $(pwd)/scripts/screenshot_full.sh $DOT_DIR/scripts/screenshot_full.sh
-cp $(pwd)/scripts/CPU_usage.sh $DOT_DIR/scripts/CPU_usage.sh
-cp $(pwd)/scripts/GPU_usage.sh $DOT_DIR/scripts/GPU_usage.sh
-cp $(pwd)/scripts/screenlock.sh $DOT_DIR/scripts/screenlock.sh
+cp $PWD/scripts/getmyip.sh $DOT_DIR/scripts/getmyip.sh
+cp $PWD/scripts/screenshot_selection.sh $DOT_DIR/scripts/screenshot_selection.sh
+cp $PWD/scripts/screenshot_full.sh $DOT_DIR/scripts/screenshot_full.sh
+cp $PWD/scripts/CPU_usage.sh $DOT_DIR/scripts/CPU_usage.sh
+cp $PWD/scripts/GPU_usage.sh $DOT_DIR/scripts/GPU_usage.sh
+cp $PWD/scripts/screenlock.sh $DOT_DIR/scripts/screenlock.sh
 
 i3-msg restart
 
