54f242ad8591eb0b8400465b1ef1b5b9ae2551a4
Berenguer Montserrat <berenguer.95@gmail.com>
2018-01-20 13:10:43 +0100

    Mem usage changes to be more correct

--- scripts/MEM_usage.sh
+++ scripts/MEM_usage.sh
@@ -7,7 +7,7 @@ total_memory=$(free -h | grep Mem: | awk '{print $2}')
 unit=${total_memory:(-1)}
 option=-$(echo $unit | awk '{print tolower($0)}')
 
-left_memory=$(free -h $option | grep Mem: | awk '{print $4}')
-left_memory=${left_memory:0:((${#left_memory}-1))}
+left_memory=$(free -h $option | grep Mem: | awk '{print $3}')
+#left_memory=${left_memory:0:((${#left_memory}-1))}
 
 echo "$left_memory""/""$total_memory"
