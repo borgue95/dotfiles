1118d1c0a6f5f1a52ac5b12ce4dbcabcfedec5d6
Berenguer Montserrat <berenguer.95@gmail.com>
2019-05-11 13:03:32 +0200

    Modified cpu and gpu status on i3blocks

--- scripts/CPU_usage.sh
+++ scripts/CPU_usage.sh
@@ -14,7 +14,7 @@ fi
 # FREQUENCY
 if [ $(lscpu | grep Architecture: | awk '{print $2}') != "x86_64" ];
 then
-    freq=$(echo "scale=2; $(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq) / 1000.0" | bc);
+    freq=$(echo "scale=1; $(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq) / 1000.0" | bc);
 else
     freq=$(lscpu | head -n 15 | tail -n 1 | awk '{print $3}')
 fi
@@ -68,17 +68,17 @@ total_load=$(($CurrTotal - $PrevTotal))
 idle_load=$(($CurrIdle - $PrevIdle))
 
 cpu_usage=$(echo "scale=6; ($total_load - $idle_load) / $total_load * 100.0" | bc)
-cpu_usage=$(echo "scale=1; $cpu_usage / 1.0" | bc | awk '{printf("%.1f", $0)}')
-cpu_usage="$cpu_usage"
+cpu_usage=$(echo "scale=1; $cpu_usage / 1.0" | bc | awk '{printf("%.0f", $0)}')
+#cpu_usage=$(printf "%8s" $cpu_usage)
+#printf "%8s\n" $cpu_usage
+#echo $cpu_usage
 
 if [ "$(echo "scale=2;$cpu_usage > 75.0 && $cpu_usage < 95.0" | bc -l)" -eq 1 ]; then # 75-95
-    output="<span foreground=\"#FFFC00\"><b>$cpu_usage%</b></span>"
-    echo -n $output
-    echo -e " @ $freq, $temp1"
+    printf "<span foreground=\"#FFFC00\"><b>%3s%%</b></span>" $cpu_usage
+    printf " @ %s, %s\n" $freq $temp1
 elif [ "$(echo "scale=2;$cpu_usage >= 95.0" | bc -l)" -eq 1 ]; then # > 95
-    output="<span background=\"#FF0000\" foreground=\"#FFFFFF\"><b> $cpu_usage% </b></span>"
-    echo -n $output
-    echo -e " @ $freq, $temp1"
+    printf "<span background=\"#FF0000\" foreground=\"#FFFFFF\"><b>%3s%%</b></span>" $cpu_usage
+    printf " @ %s, %s\n" $freq $temp1
 else # < 75
-    echo -e "$cpu_usage""% @ $freq, $temp1"
+    printf "%3s%% @ %s, %s\n" $cpu_usage $freq $temp1
 fi
